286a0859d99b787097aa252386099a2e5d39f182
Aanya <aanya.malyala@gmail.com>
2025-07-24 15:23:48 -0400

    Added about view and contact view screens.

---
+++ About View.swift
@@ -0,0 +1,18 @@
+//
+//  About View.swift
+//  Navigation
+//
+//  Created by Aanya Malyala on 7/24/25.
+//
+
+import SwiftUI
+
+struct About_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    About_View()
+}
