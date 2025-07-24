80a27d8c23464be15542839d3b47adc3ac05e240
Aanya <aanya.malyala@gmail.com>
2025-07-24 15:16:02 -0400

    Added text, button, and navigation title. Added second screen view with text.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                Text("This is the root view 🌳")
+                NavigationLink(destination: Text("You've arrived to the Second View")) {
+                    Text("Click me!")
+                }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            //.navigationBarHidden(true)
         }
-        .padding()
     }
 }
 
